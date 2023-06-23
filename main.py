dd0869fbcf462e46d97125a25fd98b337b944899
DiegoGCh <dguerrachevarria@gmail.com>
2023-06-23 06:09:45 -0500

    changed main

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ textos = ["libro1.txt","libro2.txt","libro3.txt","libro4.txt","libro5.txt","libr
 test = InvertIndex("prueba",300000,"Data.csv")
 #test.SPIMIConstruction()
 merge_blocks = test.index_blocks()
-test.write_index_tf_idf(merge_blocks, test.path_index, len(merge_blocks))
+test.write_index_tf_idf(merge_blocks, len(merge_blocks))
 #test.save_index("index_file")
 #print(test.retrieval("gandalf y los anillos", 3))
